2f99b5b9ada580aa29840f781e35b2495680e83f
AnishBanerji <anishban@buffalo.edu>
2025-04-21 06:03:23 -0400

    Implemented Logging. Added volumes to docker-compose. Appended requirements.txt

--- app/server.py
+++ app/server.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask, request, send_file, abort
+import datetime
 
 app = Flask(__name__)
+
+@app.before_request
+def log_req():
+    dt = datetime.datetime.now()
+    dt=dt.strftime('%m-%d-%Y %H-%M-%S')
+    ip = request.remote_addr
+    method = request.method
+    path = request.path
+    log = f'[{dt}]: {ip} {method} {path} \n'
+    with open('./logs/server.log','a') as f:
+        f.write(log)
